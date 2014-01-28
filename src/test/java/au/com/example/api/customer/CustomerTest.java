58ab168e1d8de898e120e4f64dd4bc6d0a4b5c0b
Robert Leggett <robert.leggett@uxcps.com.au>
2014-01-28 11:21:17 +1100

    * Added Mockito * Added intergration test using mockito and jersey test framework

--- src/test/java/au/com/example/api/customer/CustomerTest.java
+++ src/test/java/au/com/example/api/customer/CustomerTest.java
@@ -5,26 +5,65 @@ import static org.junit.Assert.assertEquals;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import au.com.example.service.customer.CustomerService;
+import au.com.example.service.customer.data.CustomerEntity;
 
 public class CustomerTest extends JerseyTest {
-	
+
+	@Mock
+	private CustomerService serviceMock;
+
 	@Override
-    protected Application configure() {
-        enable(TestProperties.LOG_TRAFFIC);
-        enable(TestProperties.DUMP_ENTITY);
-        
-		return new ResourceConfig(CustomerResource.class); 
-    }
+	protected Application configure() {
+		enable(TestProperties.LOG_TRAFFIC);
+		enable(TestProperties.DUMP_ENTITY);
+		
+		ResourceConfig config = new ResourceConfig(CustomerResource.class);
+		config.register(new InjectableProvider());
+
+		return config;
+	}
 
 	@Test
 	public void testCustomerRetrieveResponse() {
 		
-		Response response = target().path("jrws").path("rest/customer/retrieve").queryParam("id", 2).request().get();
+		long id = 2;
 		
-        assertEquals(200, response.getStatus());
+		Mockito.when(serviceMock.getCustomer(Mockito.anyLong())).thenReturn(getMockCustomer(id));
+
+		Response response = target("customer/retrieve").queryParam("id", id).request().get();
+
+		assertEquals(200, response.getStatus());
+	}
+	
+	// ======= Mocking ==========
+	
+	private CustomerEntity getMockCustomer(Long id) {
+		return new CustomerEntity(id, "Test", "Customer");
+	}
+
+	class InjectableProvider extends AbstractBinder implements Factory<CustomerService> {
+		
+		@Override
+		protected void configure() {
+			bindFactory(this).to(CustomerService.class);
+		}
+
+		public CustomerService provide() {
+			return serviceMock;
+		}
+
+		public void dispose(CustomerService service) {
+			serviceMock = null;
+		}
 	}
 }
